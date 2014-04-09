52ba5115301f2171a7b4b8bacea27ba2ea8eea9f
Arsenal <Arsenal@192.168.254.81>
2014-04-09 17:58:07 +0700

    javabasic

---
+++ javabasic/src/javabasic/MichealOwen.java
@@ -0,0 +1,46 @@
+package javabasic;
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class MichealOwen {
+	private String name;
+	private String position;
+	private String freind[];
+	
+	public MichealOwen(){
+		 name = "Michael Owen";
+		 position = "vanguard";
+		 freind = new String[]{"rio ferdinand","steven gerrard","devil beckham","frank lampard"};
+		 
+	}
+	public String getname(){
+		
+	return name;
+	}
+	
+	public String getposition(){
+		
+	return position;
+		}
+	
+	public String[] getfreind(){
+		
+		return freind;
+			}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		MichealOwen Sc = new MichealOwen();
+		String name = Sc.getname();
+		String position = Sc.getposition();
+		String freind[] = Sc.getfreind();
+		System.out.println("Name " + name);
+		System.out.println("Position " + position);
+		System.out.println("Freind are " + Arrays.toString(freind));
+	}
+
+}
